bff624b4e155a62063d80e0d685e9d85f229ad14
Gaurav Gupta <1276964+ggthedev@users.noreply.github.com>
2023-07-09 16:15:11 +0530

    Added base version of script

---
+++ generate_ssh_key.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Welcome to ssh-key generation"
+CURRENT_LOCATION=$PWD
+echo "You are currently in: $CURRENT_LOCATION"
+EXPECTED_LOC=$HOME/.ssh
+if test "$CURRENT_LOCATION" != "$EXPECTED_LOC"
+then
+    echo "You need to be in: $EXPECTED_LOC"
+    exit
+fi
+
+echo "Proceeding further...."
+#ssh-keygen -t ed25519 -N "" -C "your_email@example.com" -f n -q
+echo "Enter the desired file name without any extension "
+read FILE_NAME
+echo "Confirming the file name is: $FILE_NAME"
+echo "Enter the comment [email address] "
+read COMMENT
+echo "Enter the desired password: "
+read PASSWORD
+echo "Generating the key..."
+ssh-keygen -q -t ed25519 -N "$PASSWORD" -C "$COMMENT" -f "$FILE_NAME"
