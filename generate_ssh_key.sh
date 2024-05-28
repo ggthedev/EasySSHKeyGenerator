1d36cdfd28f1de7b0d636e3720c0e5838c462ece
Gaurav Gupta <systems.gaurav.gupta@gmail.com>
2024-05-28 16:18:03 +0530

    updated with refactor of code

--- generate_ssh_key.sh
+++ generate_ssh_key.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
 
-echo "Welcome to ssh-key generation"
-CURRENT_LOCATION=$PWD
-echo "You are currently in: $CURRENT_LOCATION"
-EXPECTED_LOC=$HOME/.ssh
-if test "$CURRENT_LOCATION" != "$EXPECTED_LOC"; then
-    echo "You need to be in: $EXPECTED_LOC"
-    exit
-fi
+check_and_set_keys_location() {
+    ssh_key_dir="SSHKEY"
+    if [ "$CURRENT_LOCATION" != "$EXPECTED_LOC" ]; then
+        #following: read only 1 character
+        read -r -n 1 -p "Switch to expected $EXPECTED_LOC :[Y|n]?" SHOULD_SWITCH
 
-echo "Proceeding further...."
-#ssh-keygen -t ed25519 -N "" -C "your_email@example.com" -f n -q
-echo "Enter the desired file name without any extension "
-read FILE_NAME
-echo "Confirming the file name is: $FILE_NAME"
-echo "Enter the comment [email address] "
-read COMMENT
-echo "Enter the desired password: "
-read PASSWORD
-echo "Generating the key..."
-ssh-keygen -q -t ed25519 -N "$PASSWORD" -C "$COMMENT" -f "$FILE_NAME"
+        if [ -z "$SHOULD_SWITCH" ] || [ "$SHOULD_SWITCH" = "y" ] || [ "$SHOULD_SWITCH" = "Y" ]; then
+            cd "$EXPECTED_LOC" || echo "Unable to change directory"
+            echo -e "\nCurrently in : $PWD"
+        else
+            echo -e "\nGenerating in current folder: $PWD"
+            mkdir -p "$PWD"/"$ssh_key_dir"
+            cd "$ssh_key_dir" || echo "Unable to change directory"
+        fi
+    fi
+}
+
+generate_key_ed25519() {
+    echo -E "Proceeding to generate key ...."
+    read -rp "Enter the desired file name without any extension " FILE_NAME
+    echo "Confirming the file name is: $FILE_NAME"
+    read -rp "Enter the comment [email address|identifying host] " COMMENT
+    read -rs -p "Enter the desired password: " PASSWORD
+    echo "Generating the key [default cipher ed25519].."
+    ssh-keygen -t ed25519 -N "$PASSWORD" -C "$COMMENT" -f "$FILE_NAME"
+}
+
+main() {
+    echo "Welcome to ssh-key generation"
+
+    CURRENT_LOCATION=$PWD
+    echo "You are currently in: $CURRENT_LOCATION"
+
+    EXPECTED_LOC=$HOME/.ssh
+    echo "Default Location of keys: $EXPECTED_LOC"
+    check_and_set_keys_location
+    generate_key_ed25519
+}
+
+# entry point to  script
+main
